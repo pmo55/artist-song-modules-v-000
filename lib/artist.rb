92f52da82f0d645a13c74fea4e4fa90a6b63a14f
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-27 21:01:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 require 'pry'
 class Artist
 
-  attr_accessor :name
-  attr_reader :songs
+  attr_accessor :name, :songs
+
 extend Memorable::ClassMethods
 extend Findable::ClassMethods
 include Paramable::InstanceMethods
